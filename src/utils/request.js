89f9ff102a4ab09e205fea72f7fb56ad3cb671f2
hao.dai <505159718@qq.com>
2018-09-28 15:01:59 +0800

    修改request文件

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,63 +3,65 @@ import config from './config'
 // const baseURL = 'http://www.ciyun.com'
 
 export const request = (url, method, data, needToken) => {
-    return new Promise((resolve, reject) => {
-        let token = wx.getStorageSync('token')
-        if (!needToken) { // 不需要token时
-            unExpectToken(url, method, data, resolve, reject)
-        } else { // 需要token时
-            if (!token) {
-                wx.removeStorageSync('token')
-                wx.redirectTo({
-                    url: '/pages/token/main'
-                })
-            } else {
-                expectToken(url, method, data)
-            }
-        }
-    })
+  return new Promise((resolve, reject) => {
+    let token = wx.getStorageSync('token')
+    if (!needToken) { // 不需要token时
+      unExpectToken(url, method, data, resolve, reject)
+    } else { // 需要token时
+      if (!token) {
+        wx.removeStorageSync('token')
+        wx.redirectTo({
+          url: '/pages/token/main'
+        })
+      } else {
+        expectToken(url, method, data, resolve, reject)
+      }
+    }
+  })
 }
-export const unExpectToken = (url, method, data, resolve, reject) => { // 不需要Token
-    wx.request({
-        url: config.request.baseURL + url, // 接口地址,
-        data: data,
-        method: method,
-        header: {
-            'content-type': config.request.header
-        },
-        success(res) {
-            resolve(res)
-        },
-        fail(ret) {
-            reject(ret)
-        }
-    })
+ // 不需要Token
+export const unExpectToken = (url, method, data, resolve, reject) => {
+  wx.request({
+    url: config.request.baseURL + url, // 接口地址,
+    data: data,
+    method: method,
+    header: {
+      'content-type': config.request.header
+    },
+    success(res) {
+      resolve(res)
+    },
+    fail(ret) {
+      reject(ret)
+    }
+  })
 }
+ // 需要Token
 export const expectToken = (url, method, data, resolve, reject) => {
-    wx.request({
-        url: config.request.baseURL + url, // 接口地址,
-        data: data,
-        method: method,
-        header: {
-            'content-type': config.request.header,
-            Authorization: config.request.Authorization
-        },
-        success(res) {
-            if (res.statusCode === 403) {
-                wx.redirectTo({
-                    url: '/pages/bindMobile/main'
-                })
-            } else if (res.statusCode === 401) {
-                wx.removeStorageSync('token')
-                wx.redirectTo({
-                    url: config.request.invaidToken
-                })
-            } else {
-                resolve(res)
-            }
-        },
-        fail(ret) {
-            reject(ret)
-        }
-    })
+  wx.request({
+    url: config.request.baseURL + url, // 接口地址,
+    data: data,
+    method: method,
+    header: {
+      'content-type': config.request.header,
+      Authorization: wx.getStorageSync('token')
+    },
+    success(res) {
+      if (res.statusCode === 403) {
+        wx.redirectTo({
+          url: '/pages/bindMobile/main'
+        })
+      } else if (res.statusCode === 401) {
+        wx.removeStorageSync('token')
+        wx.redirectTo({
+          url: config.request.invaidToken
+        })
+      } else {
+        resolve(res)
+      }
+    },
+    fail(ret) {
+      reject(ret)
+    }
+  })
 }
