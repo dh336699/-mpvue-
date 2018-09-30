8c4e5a82c3555bbefb57df491e4dbaa2ef056461
hao.dai <505159718@qq.com>
2018-09-30 17:24:55 +0800

    增加七牛云上传图片

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,49 @@
 <script>
-import store from '@/store/index'
+import store from "@/store/index";
 export default {
-    // computed: {
-    //   ...mapGetters({
-    //     getToken: 'getToken'
-    //   })
-    // },
+  // computed: {
+  //   ...mapGetters({
+  //     getToken: 'getToken'
+  //   })
+  // },
   data() {
     return {
-      tokens: ''
-    }
+      tokens: ""
+    };
   },
   mounted() {
-      // console.log('token', store.getters.getToken)
-    this._getToken()
+    // console.log('token', store.getters.getToken)
+    this._getToken();
   },
   methods: {
     _getToken() {
-      let token = store.getters.getToken
-        // let tokens = wx.getStorageSync('token')
-        // console.log(store.getters.getToken)
+      let token = store.getters.getToken;
+      // let tokens = wx.getStorageSync('token')
+      // console.log(store.getters.getToken)
       if (!token) {
-        console.log(11)
+        console.log(11);
         wx.reLaunch({
-          url: './pages/token/main'
-        })
+          url: "./pages/token/main"
+        });
       }
     }
   }
-}
+};
 </script>
 
 <style>
-  page {
-    height: 100%;
-    background: #EFEFF4;
-  }
-  ::-webkit-scrollbar {
-    width: 0;
-    height: 0;
-    color: transparent;
-  }
+page {
+  height: 100%;
+  background: #efeff4;
+}
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+  color: transparent;
+}
+.gray {
+  background: rgba(196, 196, 196, 0.4);
+}
 .container {
   height: 100%;
   display: flex;
@@ -56,24 +59,24 @@ export default {
   font-size: 36rpx;
   color: RGBA(187, 187, 187, 1);
 }
-  .choose {
-    color: #BBBBBB;
-  }
+.choose {
+  color: #bbbbbb;
+}
 
-  .wx-switch-input{
-    width:79rpx !important;
-    height:48rpx !important;
-  }
-  /*白色样式（false的样式）*/
-  .wx-switch-input::before{
-    width:79rpx !important;
-    height:48rpx !important;
-  }
-  /*绿色样式（true的样式）*/
-  .wx-switch-input::after{
-    width: 43rpx !important;
-    height: 43rpx !important;
-  }
+.wx-switch-input {
+  width: 79rpx !important;
+  height: 48rpx !important;
+}
+/*白色样式（false的样式）*/
+.wx-switch-input::before {
+  width: 79rpx !important;
+  height: 48rpx !important;
+}
+/*绿色样式（true的样式）*/
+.wx-switch-input::after {
+  width: 43rpx !important;
+  height: 43rpx !important;
+}
 /* this rule will be remove */
 * {
   transition: width 2s;
