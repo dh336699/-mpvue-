e441d752d24acc73dd165327fe9c7bc54c2ca376
hao.dai <505159718@qq.com>
2018-11-14 16:48:42 +0800

    11/14

--- src/common/js/regexp.js
+++ src/common/js/regexp.js
@@ -4,3 +4,7 @@ export let idReg = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/
 export let telReg = /^1[2-9][0-9]{9}$/
 // 订单编号
 export let orderNo = /^[A-Za-z0-9]+$/g
+// 匹配字符
+export let reg1 = /\W+/g
+// 匹配汉字
+export let reg2 = /^[\u2E80-\u9FFF]+$/g
